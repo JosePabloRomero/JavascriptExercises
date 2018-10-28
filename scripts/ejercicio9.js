900f760081e32adc8fdf9d59fac810f6f2518068
Jose Pablo <josepablo.developer@gmail.com>
2018-10-28 18:02:40 -0500

    Implementacion del ejercicio 9

--- scripts/ejercicio9.js
+++ scripts/ejercicio9.js
@@ -1,7 +1,25 @@
 function Calcular() {
   var vector = new Array(10);
+  var aux;
+  var texto = "";
 
   for (var i = 0; i < vector.length; i++) {
    vector[i] = Math.round(Math.random() * (1000 - 100)) + 100;
   }
+
+  for (var i = 0; i < vector.length; i++) {
+    for (var j = i+1; j < vector.length; j++) {
+      if (vector[j]>vector[i]) {
+        aux = vector[j];
+        vector[j] = vector[i];
+        vector[i] = aux;
+      }
+    }
+  }
+
+  for (var i = 0; i < vector.length; i++) {
+    texto = (texto+" # "+vector[i]);
+  }
+  document.getElementById("txtNumerosTitulo1").innerHTML = "<b>Lista de numeros aleatorios ordenados de menor a mayor: </>";
+  document.getElementById("txtNumeros").innerHTML = texto;
 }
